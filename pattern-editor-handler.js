ef65e9173c0b77fd9071a8f438919648209a67f2
samkent <kent_sam@outlook.com>
2019-12-24 17:54:46 +0000

    Comit for now, will get onto getting this working after christmas.

--- pattern-editor-handler.js
+++ pattern-editor-handler.js
@@ -4,21 +4,3 @@
 //Program written by Samuel Kent to handle the light pattern editor.
 //Started on Sunday, 22nd December 2019
 //Not finished.
-
-//===== Constants =====
-const $STAGE_INPUT = document.getElementById("stage-input");
-
-//===== Change the number of stages =====
-function changeStages() {
-    let output = "";
-    for (let i = 0; i < parseInt($STAGE_INPUT.value); i++) {
-        output = output + "<div class='stage'></div>";
-    }
-    console.log("Hello!")
-}
-
-//===== Assign updates to events =====
-$STAGE_INPUT.addEventListener("onkeypress", changeStages());
-
-//===== Run change stages to update at the start =====
-changeStages();
